7bcdf7b37ce0c61c67dd832aac94c5e1d7ee2f54
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-20 11:41:37 +0530

    palindrome code for three digit

--- pal.sh
+++ pal.sh
@@ -1,1 +1,21 @@
 echo "welcome"
+#! /bin/bash -x
+rev=""
+s=0
+for (( i=0;i<3;i++ ))
+do
+n=$((RANDOM%900 +100))
+while [ $n -gt 0 ]
+do
+
+	s=$(( $n % 10 ))
+	n=$(( $n / 10 ))
+	rev=$( echo ${rev}${s} )
+done
+if [ $n -eq $rev ]
+then
+	echo "$temp is a palindrome Number"
+else
+	echo "$temp is not a palindrome number"
+fi
+done
